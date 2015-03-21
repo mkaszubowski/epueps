64359b661c1da4290b7a2638406014372cd199a0
mkaszubowski <maciej@mkaszubowski.pl>
2015-03-21 12:46:43 +0100

    homepage spec update

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,22 +6,27 @@ describe "StaticPages" do
   
   describe "Home page" do
     let!(:user)     { FactoryGirl.create(:user) } 
-  	let!(:subject1) { FactoryGirl.create(:subject, popularity: 3) }
-    let!(:subject2) { FactoryGirl.create(:subject, popularity: 5) }
-    let!(:article)  { FactoryGirl.create(:article, user_id: user.id ) }
 
+  	let!(:subject1) { FactoryGirl.create(:subject, name: 's1', popularity: 3, status: 'published') }
+    let!(:subject2) { FactoryGirl.create(:subject, name: 's2', popularity: 5, status: 'published') }
+    let!(:deleted_subject) { FactoryGirl.create(:subject, name: 'del', popularity: 7, status: 'deleted') }
+    let!(:draft_subject) { FactoryGirl.create(:subject, name: 'draft', popularity: 8, status: 'draft') }
+
+    let!(:article)  { FactoryGirl.create(:article, user_id: user.id ) }
 
   	before { visit root_path }
 
   	it { should have_title "Platforma ePUEPS" }
-  	it { should have_content "Witamy" }
   	it { should have_link "Przeglądaj kursy", href: subjects_path }
   	it { should have_link "Zaloguj się", new_user_session_path }
 
   	it { should have_selector("h1", text: "Dostępne kursy") }
   	it { should have_link "Wszystkie kursy", href: subjects_path }
 
-  	it { should have_selector("h2", text: subject1.name) }
+    it { should have_selector("h2", text: subject1) }
+    it { should have_selector("h2", text: subject2) }
+    it { should_not have_selector("h2", text: draft_subject) }
+  	it { should_not have_selector("h2", text: deleted_subject) }
   	it { should have_content subject1.description }
   	it { should have_selector("img") }
   	it { should have_link "Zobacz kurs" }
@@ -29,9 +34,10 @@ describe "StaticPages" do
     it { should have_content "Najnowsze artykuły" }
     it { should have_content article.title }
     it { should have_content article.body }
+    it { should have_link 'Wszystkie artykuły', href: articles_path }
 
   	it "should have subjects ordered by popularity" do
-  		expect(Subject.popular).to eq [subject2, subject1]
+  		expect(page.text).to match /#{subject2}.*#{subject1}/
   	end
 
     describe "'Zobacz kurs' button" do
