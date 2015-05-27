35b69fe8b223013d9828c42c58907acd64905651
mkaszubowski <maciej@mkaszubowski.pl>
2015-05-27 23:04:36 +0200

    debug react components in production

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,4 +1,12 @@
 React = require('react');
 
+console.log('loaded react');
+console.log(React);
+
 Subject = require('./components/subject.js.jsx.coffee');
+
+console.log('loaded subjects');
+console.log(Subject);
 Subjects = require('./components/subjects.js.jsx.coffee');
+console.log('loaded subjects');
+console.log(Subjects);
