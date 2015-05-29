7cdcd29e494267d4b41a9814b5ea22d2b7c87c99
mkaszubowski <maciej@mkaszubowski.pl>
2015-05-29 19:42:25 +0200

    Revert "try to fix react in production"
    This reverts commit ca86079ee54e25c84032332c3abd56da182929ca.
    
    Conflicts:
    	app/assets/javascripts/components.js

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,1 +1,12 @@
-//= require_tree ./components
+React = require('react');
+
+console.log('loaded react');
+console.log(React);
+
+Subject = require('./components/subject.js.jsx.coffee');
+
+console.log('loaded subjects');
+console.log(Subject);
+Subjects = require('./components/subjects.js.jsx.coffee');
+console.log('loaded subjects');
+console.log(Subjects);
