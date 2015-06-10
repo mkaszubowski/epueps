4961f3afd7c24c59cde987edbe7fb4221aaea167
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-06-10 18:04:28 +0200

    fix google map (turbolinks issue)

--- app/assets/javascripts/contact_map.js
+++ app/assets/javascripts/contact_map.js
@@ -1,24 +1,21 @@
 $ = jQuery = require('jquery');
 
-$(document).ready(function() {
-  function initialize() {
-    var mapOptions = {
-      center: { lat: 52.397036, lng: 16.965626},
-      zoom: 14,
-      scrollwheel: false,
-      zIndex: 1
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+var ready = function() {
+  var mapOptions = {
+    center: { lat: 52.397036, lng: 16.965626},
+    zoom: 14,
+    scrollwheel: false,
+    zIndex: 1
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 
-    var marker = new google.maps.Marker({
-      position: { lat: 52.397874, lng: 16.951722},
-      map: map,
-      title: 'Hello World!'
-    });
+  var marker = new google.maps.Marker({
+    position: { lat: 52.397874, lng: 16.951722},
+    map: map,
+    title: 'Hello World!'
+  });
+};
 
-  }
-
-  console.log('map')
-  google.maps.event.addDomListener(window, 'load', initialize);
-});
+$(document).ready(ready);
+$(document).on('page:load', ready);
