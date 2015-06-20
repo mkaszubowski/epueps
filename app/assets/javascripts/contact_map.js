34ab2bac08dfb525e121162158f85269da2108fb
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-06-20 14:06:23 +0200

    prevent errors from map javascript on other pages

--- app/assets/javascripts/contact_map.js
+++ app/assets/javascripts/contact_map.js
@@ -1,23 +1,27 @@
 $ = require('jquery');
 
-var ready = function() {
-  var mapOptions = {
-    center: { lat: 52.397036, lng: 16.965626},
-    zoom: 14,
-    scrollwheel: false,
-    zIndex: 1
+if($('#map-canvas').length > 0) {
+
+  var ready = function() {
+    var mapOptions = {
+      center: { lat: 52.397036, lng: 16.965626},
+      zoom: 14,
+      scrollwheel: false,
+      zIndex: 1
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+      position: { lat: 52.397874, lng: 16.951722},
+      map: map,
+      title: 'Hello World!'
+    });
+
+    console.log('ready-map');
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
 
-  var marker = new google.maps.Marker({
-    position: { lat: 52.397874, lng: 16.951722},
-    map: map,
-    title: 'Hello World!'
-  });
+  $(document).ready(ready);
+  $(document).on('page:load', ready);
 
-  console.log('ready-map');
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
+}
