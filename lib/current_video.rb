2691e2bef1a304aab3f9dd3f02e922f3cf0b38f8
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-08-27 19:40:50 +0200

    add header to subjects#show page

--- lib/current_video.rb
+++ lib/current_video.rb
@@ -1,18 +1,18 @@
 module CurrentVideo
 
   # returns lesson's current or
-  # first video if lesson hadn't been played yet 
+  # first video if lesson hadn't been played yet
 
   def current_video(subject)
-    video_id = 
-      params[:video_id]                           || 
-      current_video_from_cookie(subject)   || 
+    video_id =
+      params[:video_id] ||
+      current_video_from_cookie(subject) ||
       nil
 
     if video_id
       Video.find(video_id)
     else
-      subject.lessons.first.videos.first unless subject.lessons.empty? 
+      subject.lessons.first.videos.first unless subject.lessons.empty?
     end
   end
 
@@ -21,7 +21,7 @@ module CurrentVideo
   end
 
   def create_example_video(lesson)
-    lesson.videos.create(name: "example", 
+    lesson.videos.create(name: "example",
       link: "https://youtube.com/watch?v=example")
   end
 
