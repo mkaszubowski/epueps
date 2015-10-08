00933efea6b7be350f5659ca7faa0ea4c299de72
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-08 19:42:51 +0200

    change articles displaying on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @splash = true unless user_signed_in?
     @subjects = Subject.published.popular.limit(3)
-    @articles = Article.includes(:user).limit(3)
+    @articles = Article.limit(3).decorate
+    @recommended_article = Article.first.decorate
   end
 
   def contact
