58d2c7b19441125d95f9f6abe76a502c2b9ea01f
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-09 20:12:07 +0200

    change articles query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @splash = true unless user_signed_in?
     @subjects = Subject.published.popular.limit(3)
-    @articles = Article.limit(3).decorate
-    @recommended_article = Article.first.decorate
+    @articles = Article.includes(:user).newest.limit(4).decorate
   end
 
   def contact
