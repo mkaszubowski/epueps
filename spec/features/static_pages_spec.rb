a0cd693cd925287ffe2fd469f14e4d144a761dcb
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-10 14:09:52 +0200

    fix static pages spec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,31 +1,39 @@
 require 'rails_helper'
 
-describe "StaticPages" do
+describe 'Static Pages', type: :feature do
 
-	subject { page }
-
-  describe "Home page" do
+  describe 'Home page' do
     let!(:user)     { FactoryGirl.create(:user) }
 
-  	let!(:subject1) { FactoryGirl.create(:subject, name: 's1', popularity: 3, status: 'published') }
-    let!(:subject2) { FactoryGirl.create(:subject, name: 's2', popularity: 5, status: 'published') }
-    let!(:deleted_subject) { FactoryGirl.create(:subject, name: 'del', popularity: 7, status: 'deleted') }
-    let!(:draft_subject) { FactoryGirl.create(:subject, name: 'draft', popularity: 8, status: 'draft') }
+    let(:now) { DateTime.now }
+    let!(:article1) do
+      FactoryGirl.create(:article, user_id: user.id, created_at: now)
+    end
 
-    let!(:article)  { FactoryGirl.create(:article, user_id: user.id ) }
+    let!(:article2) do
+      FactoryGirl.create(:article, body: 'article2', user_id: user.id, created_at: now - 1)
+    end
 
-  	before { visit root_path }
+    before { visit root_path }
 
-  	it { should have_title "Platforma ePUEPS" }
-  	it { should have_link "Przeglądaj kursy", href: subjects_path }
-  	it { should have_link "Zaloguj się", new_user_session_path }
+    it 'should have sign in link' do
+      expect(page).to have_xpath("//a[@href='/users/sign_in']")
+    end
 
-  	it { should have_selector("h1", text: "Dostępne kursy") }
-  	it { should have_link "Wszystkie kursy", href: subjects_path }
+    describe 'articles section' do
+      it 'should display first article\'s title and body' do
+        expect(page).to have_content Article.first.title
+        expect(page).to have_content Article.first.body
+      end
 
-    it { should have_content "Najnowsze artykuły" }
-    it { should have_content article.title }
-    it { should have_content article.body }
-    it { should have_link 'Wszystkie artykuły', href: articles_path }
+      it 'should display only title for other articles' do
+        expect(page).to have_content article2.title
+        expect(page).not_to have_content article2.body
+      end
+
+      it 'should display link to all articles' do
+        expect(page).to have_link 'Wszystkie artykuły', href: articles_path
+      end
+    end
   end
 end
